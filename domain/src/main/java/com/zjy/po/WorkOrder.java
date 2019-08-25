0b26778e11de4db4380f5085b572ad11eb788a51
chahongjing <310510906@qq.com>
2019-08-25 13:55:45 +0800

    公共service, dao集成

---
+++ domain/src/main/java/com/zjy/po/WorkOrder.java
@@ -0,0 +1,38 @@
+package com.zjy.po;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author jyzeng
+ * @date 2019-08-25 12:47:50
+ */
+public class WorkOrder implements Serializable {
+    /**
+     * 
+     */
+    private Long id;
+
+    /**
+     * 
+     */
+    private String num;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num == null ? null : num.trim();
+    }
+}
