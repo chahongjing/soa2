e3cdc9db00bf6376e9a5a154b706bccbd20d94f1
junyi.zeng <junyi.zeng@dmall.com>
2019-10-25 09:22:35 +0800

    添加springboot admin模块

---
+++ springboot-admin/src/test/java/com/zjy/springbootadmin/SpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjy.springbootadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
